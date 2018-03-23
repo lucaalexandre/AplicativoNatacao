6fd8aa69fb457302d99ecc326f0480bdc1cd8bcc
logonrm <logonrm@fiap.com.br>
2018-03-23 09:40:07 -0300

    Initial commit

---
+++ app/src/main/java/com/fiap/aplicativonatacao/TelaActivity.kt
@@ -0,0 +1,12 @@
+package com.fiap.aplicativonatacao
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela)
+    }
+}
